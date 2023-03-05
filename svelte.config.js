d0d065052a97cb6610839143a8197c7e64023e4c
Adamparishspok <parishadam2@gmail.com>
2023-03-05 11:38:33 -0800

    1 file modified

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,6 @@ import node from '@sveltejs/adapter-node';
 const config = {
 	kit: {
 		adapter: node()
-	},
-	preprocess: {
-		// ...
-	},
-	// specify the output directory for the build
-	paths: {
-		base: '/my-app',
-		assets: '/my-app',
-		ssr: 'functions',
-		output: '__sapper__/build'
 	}
 };
 
